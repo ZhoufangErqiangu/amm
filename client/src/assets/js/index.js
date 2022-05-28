18ecaed2c12094506509d85145f62caeab416bd8
alex <liuhlightning@qq.com>
2022-05-28 22:23:49 +0800

    rpc

--- client/src/assets/js/index.js
+++ client/src/assets/js/index.js
@@ -1,1 +1,3 @@
-export const rpcUrl = 'http://localhost:8899';
+// export const rpcUrl = "https://solana-api.projectserum.com/";
+export const rpcUrl = "https://api.devnet.solana.com/";
+// export const rpcUrl = "http://localhost:8899";
